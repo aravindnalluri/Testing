8c20f3047c664f113d93dd6157ede915e666331d
aravind nalluri <aravindsfdc47@gmail.com>
2019-11-14 22:42:37 -0500

    added class

---
+++ happy/src/happy/testing.java
@@ -0,0 +1,17 @@
+package happy;
+import java.util.Scanner;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner S=new Scanner(System.in);//creating object of Scanner class
+		int a,b,c;
+		System.out.println("Enter 2 values ");
+		a=S.nextInt();
+		b=S.nextInt();
+		c=a+b;
+		System.out.println("Sum is "+c);
+	}
+
+}
